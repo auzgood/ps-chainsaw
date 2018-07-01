37a4bcdfd193d9eb6cdac70c69b955291e804131
auZ <xpcaustin@gmail.com>
2018-06-30 21:13:42 -0700

    updated remove win10 junk
    Added read from text file & added folder sructure

---
+++ RemoveWin10Junk/Win10Junk_Uninstall.ps1
@@ -0,0 +1,29 @@
+﻿# 
+
+ 
+## Configure the apps to be removed
+# add to list use : Get-AppxPackage * | select name
+
+$applist = Import-Csv -path .\apps.txt
+ 
+##Remove the Apps listed above or report if app not present
+ForEach ($App in $AppsList)
+{
+    $PackageFullName = (Get-AppxPackage $App).PackageFullName
+ 
+    If ($PackageFullName)
+    {
+        Write-Host "Removing Package: $App"
+        Remove-AppxPackage -Package $PackageFullName
+    }
+ 
+    Else
+    {
+        Write-Host "Unable to find package: $App"
+    }
+}
+ 
+## End
+
+
+
